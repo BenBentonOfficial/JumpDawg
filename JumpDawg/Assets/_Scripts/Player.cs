605eb3bb6e4da228002415ede80533a9634811dd
Benjamin Benton <benbentonofficial@gmail.com>
2024-09-19 14:42:51 -0500

    begin player setup

--- JumpDawg/Assets/_Scripts/Player.cs
+++ JumpDawg/Assets/_Scripts/Player.cs
@@ -1,16 +1,27 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    protected Rigidbody2D _rb;
 
-    // Update is called once per frame
-    void Update()
+    protected Animator _animator;
+
+    protected int facingDir = 1;
+
+    [SerializeField] protected float jumpForce;
+
+    [SerializeField] protected float groundCheckDistance;
+
+    [SerializeField] protected LayerMask groundLayer;
+    
+    public bool touchingGround => Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
+
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponentInChildren<Animator>();
+        groundLayer = LayerMask.GetMask("Ground");
     }
+    
 }
